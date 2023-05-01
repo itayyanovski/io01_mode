95396490e45ed42b77105100281105047c2eefdc
io01Demo <132198888+io01Demo@users.noreply.github.com>
2023-05-01 12:49:28 +0300

    add test

---
+++ testCommands.js
@@ -0,0 +1,16 @@
+{
+    
+    "io01_ver": 
+    {
+        "type": "run",
+        "desc": "Show kadabra test commands help", 
+        "cmd": "echo 1.0.1.0"
+    },
+
+    "io01_update": 
+    {
+        "type": "run",
+        "desc": "Update kadabra commands", 
+        "cmd": "sh ./io01_update.sh"
+    }
+}

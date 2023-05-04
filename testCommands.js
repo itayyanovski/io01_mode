075758405b2659c2eb7c0ae6a0165a09ff08c70d
io01Demo <132198888+io01Demo@users.noreply.github.com>
2023-05-04 13:05:50 +0300

    Try it yourself

--- testCommands.js
+++ testCommands.js
@@ -1,16 +1,26 @@
 {
-    
     "io01_ver": 
     {
         "type": "run",
         "desc": "Show kadabra test commands help", 
-        "cmd": "echo 1.0.1.0"
+        "cmd": "echo 1.0.1.1"
     },
-
     "io01_update": 
     {
         "type": "run",
         "desc": "Update kadabra commands", 
         "cmd": "sh ./io01_update.sh"
+    },
+    "test_cmd": 
+    {
+        "type": "run",
+        "desc": "Try it your self", 
+        "cmd": "echo Try it your self - take a test drive"
+    },
+    "last_cmd": 
+    {
+        "type": "run",
+        "desc": "Try it your self", 
+        "cmd": "echo Try it your self - take a test drive"
     }
 }
